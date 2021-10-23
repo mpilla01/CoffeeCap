24f35e71264738f78a44edfc6f7672ba92061386
rgudla2003@gmail.com <rgudla2003@gmail.com>
2021-10-23 01:44:07 -0400

    println was updated for name

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Good morning! What is your name?");
+        System.out.print("Good morning! What is your name? ");
         String name = input.next();
         input.nextLine();
-        
+
     }
 }
